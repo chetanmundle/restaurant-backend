b07c3b12d9712c333eee74ed20c1696a37b2aef3
Chaten Mundle <chetanmundle7875@gmail.com>
2024-02-28 16:33:00 +0530

    28.2.2024 4.32pm

--- resturentapplication/src/main/java/com/api/resturentapplication/entities/Resturant.java
+++ resturentapplication/src/main/java/com/api/resturentapplication/entities/Resturant.java
@@ -3,6 +3,7 @@ package com.api.resturentapplication.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
